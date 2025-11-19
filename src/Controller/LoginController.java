c00916bb656d6aa0ae8c41ca35d23d0bc7626e13
Qiwqiw-Alt <forgamefauzan10@gmail.com>
2025-11-20 04:47:10 +0700

    update Controller dan Sevice

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,4 +1,34 @@
 package Controller;
 
+import Model.Admin;
+import Model.Penyetor;
+import Model.User;
+import Service.LoginService;
+import View.LoginView;
+
 public class LoginController {
+    private LoginView view;
+    private LoginService service;
+
+    public LoginController(LoginView view, LoginService service){
+        this.view = view;
+        this.service = service;
+    }
+
+    public void login() {
+        // String username = view.getUsernameInput();
+        // String password = view.getPasswordInput();
+
+        // User user = service.login(username, password);
+
+//        if (user == null) {
+//            view.showMessage("Login gagal! Username atau password salah.");
+//        } else if (user instanceof Admin) {
+//            view.showMessage("Login sebagai Admin berhasil!");
+//            view.moveToAdminDashboard();
+//        } else if (user instanceof Penyetor) {
+//            view.showMessage("Login sebagai Penyetor berhasil!");
+//            view.moveToUserDashboard();
+//        }
+    }
 }
--- src/Service/LoginService.java
+++ src/Service/LoginService.java
@@ -1,5 +1,29 @@
 package Service;
 
-public class LoginService {
+import Model.Admin;
+import Model.Penyetor;
+import Model.User;
+import Model.BankSampah;
 
+public class LoginService {
+    private BankSampah bankSampah;
+
+    public LoginService(BankSampah bankSampah) {
+        this.bankSampah = bankSampah;
+    }
+
+    public User login(String username, String password) {
+        Admin admin = bankSampah.getAdmin();
+        if(admin.getUsername().equals(username) && admin.getPassword().equals(password)){
+            return  admin;
+        }
+
+        for(Penyetor penyetor : bankSampah.getDaftarPenyetor()){
+            if(penyetor.getUsername().equals(username) && penyetor.getPassword().equals(password)){
+                return  penyetor;
+            }
+        }
+
+        return  null;
+    }
 }
