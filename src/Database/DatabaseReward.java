2516c286bdc67a38b112642d4ed3e0ea45cd2304
Kristian WS <kristianws@gmail.com>
2025-11-22 17:36:44 +0700

    Tambah load and save data reward

--- src/Database/DatabaseReward.java
+++ src/Database/DatabaseReward.java
@@ -1,4 +1,71 @@
 package Database;
 
+import Model.Reward;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class DatabaseReward {
+    private ArrayList<Reward> listReward;
+    private static final String DATA_REWARD = "src\\Database\\Reward\\reward.txt";
+    String delim = "\\|";
+
+    public DatabaseReward() {
+        this.listReward = loadData();
+    }
+
+    public void addReward(Reward rewardBaru) {
+        listReward.add(rewardBaru);
+        saveData();
+    }
+
+    ArrayList<Reward> loadData() {
+        ArrayList<Reward> list = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(DATA_REWARD))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(delim);
+                if (data.length >= 5) {
+                    Reward loadReward = new Reward(
+                            data[0], // untuk id
+                            data[1], // nama hadiah
+                            data[2], // deskripsi
+                            Double.parseDouble(data[3]), // poin tukar
+                            Integer.parseInt(data[4]) // stok
+                    );
+                    listReward.add(loadReward);
+                }
+
+            }
+            return list;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return list;
+        }
+    }
+
+    public void saveData() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(DATA_REWARD))) {
+            
+            for (Reward reward : listReward) {
+                String data = reward.getIdReward() + "|" +
+                        reward.getNamaHadiah() + "|" +
+                        reward.getDeskripsi() + "|" +
+                        reward.getPoinTukar() + "|" + 
+                        reward.getStok() + "\n";
+
+                bw.write(data);
+            }
+            System.out.println("--- Data Reward Berhasil disimpan ke file ---");
+
+        } catch (Exception e) {
+            System.out.println("Error: Gagal menyimpan data reward ke file. " + e.getMessage());
+        }
+    }
+
 }
