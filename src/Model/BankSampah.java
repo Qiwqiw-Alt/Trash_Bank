e977aa1c5a4f64a114249d09cd02734573ce0422
Qiwqiw-Alt <forgamefauzan10@gmail.com>
2025-11-28 15:23:26 +0700

    model fx

--- src/Model/BankSampah.java
+++ src/Model/BankSampah.java
@@ -1,17 +1,10 @@
 package Model;
 
-import java.util.ArrayList;
-
 public class BankSampah {
     private String idBank;
     private String namaBankSampah;
     private String alamat;
-
     private Admin admin;
-    private ArrayList<Penyetor> daftarPenyetor;
-    private ArrayList<Sampah> daftarKategoriSampah;
-    private ArrayList<Transaksi> daftarTransaksi;
-    private ArrayList<Reward> daftarReward;
 
     private String fileAdmin;
     private String filePenyetor;
@@ -25,34 +18,20 @@ public class BankSampah {
         this.namaBankSampah = namaBank;
         this.alamat = alamat;
 
-        this.daftarPenyetor = new ArrayList<>();
-        this.daftarKategoriSampah = new ArrayList<>();
-        this.daftarTransaksi = new ArrayList<>();
-
         this.fileAdmin = "src\\Database\\Admin\\admin_" + idBank + ".txt";
         this.filePenyetor = "src\\Database\\Penyetor\\penyetor_" + idBank + ".txt";
         this.fileTransaksi = "src\\Database\\Transaksi\\trx_" + idBank + ".txt";
         this.fileDaftarSampah = "src\\Database\\Sampah\\dfsampah_" + idBank + ".txt";
         this.fileDaftarReward = "src\\Database\\Reward\\dfreward_" + idBank + ".txt";
         this.fileDaftarComplain = "src\\Database\\Complain\\dfcompalain_" + idBank + ".txt";
-
     }
 
     public String getIdBank() { return idBank; }
     public String getNamaBank() { return namaBankSampah; }
-    public void setNamaBank(String namaBank){
-        this.namaBankSampah = namaBank;
-    }
-
+    public void setNamaBank(String namaBank){ this.namaBankSampah = namaBank;}
     public Admin getAdmin() { return admin; }
-
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-
-    public String getAlamat(){
-        return this.alamat;
-    }
+    public void setAlamat(String alamat) {this.alamat = alamat;}
+    public String getAlamat(){return this.alamat;}
 
     public String getFileAdmin() { return fileAdmin; }
     public String getFilePenyetor() { return filePenyetor; }
@@ -60,6 +39,4 @@ public class BankSampah {
     public String getFilDaftarSampah() { return fileDaftarSampah; }
     public String getFileReward() { return fileDaftarReward; }
     public String getFileComplain() { return fileDaftarComplain; }
-
-    
 }
