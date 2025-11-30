2b1318afa52795d58b11756b1971cf0114aa7d3e
Qiwqiw-Alt <forgamefauzan10@gmail.com>
2025-12-01 06:10:20 +0700

    fix all controller last

--- src/Controller/TransaksiController.java
+++ src/Controller/TransaksiController.java
@@ -6,7 +6,6 @@ public class TransaksiController {
     private static TransaksiService service = new TransaksiService(); 
 
     public TransaksiController() {
-        // Kosongkan atau hapus constructor ini jika tidak dipakai
     }
 
     public static TransaksiService getService() {
